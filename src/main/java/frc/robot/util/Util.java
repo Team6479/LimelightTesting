3059387a77649433df7b0f1a852ae975e4cbfc7a
iboyperson <tjquillan@gmail.com>
2020-01-24 21:58:23 -0700

    Add getRange method to Util

--- src/main/java/frc/robot/util/Util.java
+++ src/main/java/frc/robot/util/Util.java
@@ -3,7 +3,11 @@ package frc.robot.util;
 public final class Util {
     private Util() {}
 
+    public static double getRange(double x, double y) {
+      return Math.abs(Math.abs(x) - Math.abs(y));
+    }
+
     public static boolean inRange(double x, double y, double range) {
-        return Math.abs(Math.abs(x) - Math.abs(y)) <= range;
+        return getRange(x, y) <= range;
     }
 }
